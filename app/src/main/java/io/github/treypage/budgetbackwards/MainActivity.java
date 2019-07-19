0da4b5ee88d68f16524e1de21441d797512c78b6
Terrell Page <tpage46@gmail.com>
2019-07-19 13:54:04 -0600

    added license, changed fragment manager, broke something

--- app/src/main/java/io/github/treypage/budgetbackwards/MainActivity.java
+++ app/src/main/java/io/github/treypage/budgetbackwards/MainActivity.java
@@ -1,9 +1,30 @@
+//    MIT License
+//
+//    Copyright (c) 2019 Terrell Page
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
 package io.github.treypage.budgetbackwards;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import io.github.treypage.budgetbackwards.fragment.CategoryChartFragment;
@@ -11,6 +32,7 @@ import io.github.treypage.budgetbackwards.fragment.CategoryListFragment;
 import io.github.treypage.budgetbackwards.fragment.ExpenseFragment;
 import io.github.treypage.budgetbackwards.fragment.IncomeFragment;
 import io.github.treypage.budgetbackwards.fragment.Information;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,46 +40,54 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Fragment fragment = Information.newInstance();
-    FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
-    transaction1.replace(R.id.frame_layout, fragment);
-    transaction1.commit();
+    CategoryChartFragment categoryChartFragment = new CategoryChartFragment();
+    CategoryListFragment categoryListFragment = new CategoryListFragment();
+    IncomeFragment incomeFragment = new IncomeFragment();
+    ExpenseFragment expenseFragment = new ExpenseFragment();
+    Information information = new Information();
+    getSupportFragmentManager().beginTransaction()
+        .add(R.id.frame_layout, categoryChartFragment)
+        .add(R.id.frame_layout, incomeFragment)
+        .add(R.id.frame_layout, categoryListFragment)
+        .add(R.id.frame_layout, expenseFragment)
+        .add(R.id.frame_layout, information)
+        .commitNow();
+    showFragment(information);
     BottomNavigationView navigation = findViewById(R.id.navigation);
     navigation.setOnNavigationItemSelectedListener(item -> {
-      Fragment selectedFragment;
       switch (item.getItemId()) {
         case R.id.navigation_list:
-          selectedFragment = CategoryListFragment.newInstance();
+          showFragment(categoryListFragment);
           break;
         case R.id.navigation_chart:
-          selectedFragment = CategoryChartFragment.newInstance();
+          showFragment(categoryChartFragment);
           break;
         case R.id.money_sign:
-          selectedFragment = IncomeFragment.newInstance();
+          showFragment(incomeFragment);
           break;
         case R.id.navigation_settings:
-          selectedFragment = ExpenseFragment.newInstance();
+          showFragment(expenseFragment);
           break;
         case R.id.menu_info:
-        default:
-          selectedFragment = Information.newInstance();
+          showFragment(information);
           break;
       }
-      FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-      transaction.replace(R.id.frame_layout, selectedFragment);
-      transaction.commit();
       return true;
     });
     navigation.setSelectedItemId(R.id.menu_info);
   }
 
-  @Override
-  public void onBackPressed() {
-    if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
-      getSupportFragmentManager().popBackStack();
-    } else {
-      FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-      transaction.replace(R.id.frame_layout, Information.newInstance());
-      transaction.commit();    }
+  private void showFragment(Fragment fragment) {
+    FragmentManager manager = getSupportFragmentManager();
+    List<Fragment> fragments = manager.getFragments();
+    FragmentTransaction transaction = manager.beginTransaction();
+    for (Fragment frag : fragments) {
+      if (frag == fragment) {
+        transaction.show(frag);
+      } else {
+        transaction.hide(frag);
+      }
+    }
+    transaction.commit();
   }
 }
--- app/src/main/java/io/github/treypage/budgetbackwards/fragment/ExpenseFragment.java
+++ app/src/main/java/io/github/treypage/budgetbackwards/fragment/ExpenseFragment.java
@@ -1,3 +1,24 @@
+//    MIT License
+//
+//    Copyright (c) 2019 Terrell Page
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
 package io.github.treypage.budgetbackwards.fragment;
 
 import android.content.Context;
@@ -92,14 +113,14 @@ public class ExpenseFragment extends Fragment {
   }
 
   private void checkExpense() {
-    MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-    long expenses = 0;
+    final long[] expenses = {0};
     try {
-      expenses = viewModel.getSumExpenses().getValue();
+      MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+      viewModel.getSumExpenses().observe(this, ex -> expenses[0] = ex);
     } catch (Exception e) {
       //Do Nothing
     }
-    if (expenses == 0) {
+    if (expenses[0] == 0) {
       startActivity(new Intent(getContext(), SplashIntro.class)
           .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
     }
